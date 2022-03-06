297a3a2b1b6483221fa8d433bed9eb90dd07c137
Carlos Benetti <benetticarlos1@gmail.com>
2022-03-06 18:49:57 -0300

    cors config

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
+const cors = require('cors')
 const pool = require('./database');
 
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 const app = express();
-
+app.use(cors())
 app.use(bodyParser.json());
 
 // Routes
